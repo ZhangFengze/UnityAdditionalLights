ef4040db20aea345e0e5db60afb47b00a0e70492
CrazyZ <CrazyZZZZ@ZZZZZ>
2022-04-14 22:42:28 +0800

    refactor

--- Assets/BakeLightmap/Editor/ViewLightIOverlap.cs
+++ Assets/BakeLightmap/Editor/ViewLightIOverlap.cs
@@ -4,13 +4,13 @@ using UnityEditor;
 public static class ViewLightIntersection
 {
     [MenuItem("Additional Lights/Draw Mode: Light Overlap")]
-    static void SceneViewCustomSceneMode()
+    public static void SceneViewCustomSceneMode()
     {
         SceneView.lastActiveSceneView.SetSceneViewShaderReplace(Shader.Find("Light Overlap"), null);
     }
 
     [MenuItem("Additional Lights/Draw Mode: None")]
-    static void SceneViewClearSceneView()
+    public static void SceneViewClearSceneView()
     {
         foreach (SceneView sceneView in SceneView.sceneViews)
             sceneView.SetSceneViewShaderReplace(null, null);
